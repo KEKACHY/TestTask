8793ddf3417945e0aefa700cfc1da26d040ac5a3
KEKACHY <KEKACHY@mail.ru>
2026-07-27 01:35:29 +0300

    Подготовка к базе данных

---
+++ TestTask/Models/ResultEntity.cs
@@ -0,0 +1,23 @@
+﻿namespace TestTask.Models
+{
+    public class ResultEntity
+    {
+        public int Id { get; set; }
+
+        public string FileName { get; set; } = string.Empty;
+
+        public double TimeDelta { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public double AverageExecutionTime { get; set; }
+
+        public double AverageValue { get; set; }
+
+        public double MedianValue { get; set; }
+
+        public double MaxValue { get; set; }
+
+        public double MinValue { get; set; }
+    }
+}
---
+++ TestTask/Models/ValueEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace TestTask.Models
+{
+    public class ValueEntity
+    {
+        public int Id { get; set; }
+
+        public string FileName { get; set; } = string.Empty;
+
+        public DateTime Date { get; set; }
+
+        public double ExecutionTime { get; set; }
+
+        public double Value { get; set; }
+    }
+}
